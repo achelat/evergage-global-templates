ee2f868d036f4b34d384615f5598607f9cfbf5a1
jsantacruzEG <jsantacruz@salesforce.com>
2022-03-31 16:47:26 -0400

    added sendChatbotStats function to handle campaign stats

--- salesforce-chatbot/client.js
+++ salesforce-chatbot/client.js
@@ -16,54 +16,22 @@
      * @description calls a function 'embedded_svc.inviteAPI.inviteButton.acceptInvite();'
      * to activate the chatbot session
      */
-    function openChatbot({ context }) {
-        embedded_svc.inviteAPI.inviteButton.acceptInvite();
-    }
-
-    /**
-     * @function handleTriggerEvent
-     * @param {Object} context
-     * @description Create trigger event based on context
-     */
-    function handleTriggerEvent({ context }) {
-        if (!context.contentZone) return;
-
-        const { userGroup, triggerOptions, triggerOptionsNumber } = context || {};
-
-
-        switch (triggerOptions.name) {
-            case "timeOnPage":
-                return new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        if (userGroup === "Control") return true;
-
-                        openChatbot({ context });
-                        resolve(true);
-                    }, triggerOptionsNumber);
-                });
-            case "inactivity":
-                return SalesforceInteractions.DisplayUtils
-                    .bind(buildBindId(context))
-                    .pageInactive(triggerOptionsNumber)
-                    .then(() => {
-                        if (userGroup === "Control") return true;
-
-                        openChatbot({ context });
-                    });
+    function openChatbot() {
+        if (embedded_svc) {
+            embedded_svc.inviteAPI.inviteButton.acceptInvite();
         }
     }
 
-    function apply(context) {
-        return new Promise((resolve, reject) => {
+    function sendChatbotStats({ context }) {
+        return new Promise(() => {
             SalesforceInteractions.mcis.sendStat({
                 campaignStats: [{
                     control: false,
                     experienceId: context.experience,
                     stat: "Impression"
                 }]
-            });
-            handleTriggerEvent({ context });
-            Evergage.DisplayUtils
+            })
+            SalesforceInteractions.DisplayUtils
                 .bind(buildBindId(context))
                 .pageElementLoaded("embeddedservice-chat-header")
                 .then(() => {
@@ -82,7 +50,7 @@
                 }).then(() => {
                     const inputFirstName = SalesforceInteractions.cashDom("#FirstName");
                     const inputLastName = SalesforceInteractions.cashDom("#LastName");
-
+                    //Attaches a listener to the Start Chatting button
                     SalesforceInteractions.cashDom(".startButton").on("click", () => {
                         if (inputFirstName.val().length && inputLastName.val().length > 0) {
                             SalesforceInteractions.mcis.sendStat({
@@ -96,6 +64,47 @@
                     });
                 });
         });
+    }
+
+
+    /**
+     * @function handleTriggerEvent
+     * @param {Object} context
+     * @description Create trigger event based on context
+     */
+    function handleTriggerEvent({ context }) {
+        if (!context.contentZone) return;
+
+        const { userGroup, triggerOptions, triggerOptionsNumber } = context || {};
+
+
+        switch (triggerOptions.name) {
+            case "timeOnPage":
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        if (userGroup === "Control") return true;
+
+                        openChatbot();
+                        sendChatbotStats({ context });
+                        resolve(true);
+                    }, triggerOptionsNumber);
+                });
+            case "inactivity":
+                return SalesforceInteractions.DisplayUtils
+                    .bind(buildBindId(context))
+                    .pageInactive(triggerOptionsNumber)
+                    .then(() => {
+                        if (userGroup === "Control") return true;
+
+                        openChatbot();
+                        sendChatbotStats({ context });
+                    });
+        }
+    }
+
+    function apply(context) {
+        return handleTriggerEvent({ context });
+
     }
 
     function reset(context) {
