526396cf3dcdeb8da72bcb3dd4a60ab17aed3b5e
Andy Maskiell <amaskiell@salesforce.com>
2022-05-03 14:47:26 -0400

    Add several helper functions, refactor handling test experience and implement handling control experience

--- redirect/client.js
+++ redirect/client.js
@@ -1,47 +1,85 @@
 (function() {
 
-    function apply(context, template) {
+    /**
+     * @function isEditorFramePresent
+     * @returns {boolean}
+     * @description Return true when IS Launcher element is present
+     */
+    function isEditorFramePresent() {
+        return (window.frameElement || {}).id === "siteEditorFrame";
+    }
 
-        /** Prevent redirect from occurring while in either the Template Editor or Campaign Editor. */
-        if ((window.frameElement || {}).id === "siteEditorFrame") {
-            return;
-        }
-
+    /**
+     * @function shouldPreventRedirect
+     * @param {Object} context
+     * @returns {boolean}
+     * @description Return true if the execution of the redirect should be prevented
+     */
+    function shouldPreventRedirect(context) {
         const currentPage = window.location.hostname + window.location.pathname.replace(/\/$/, "");
         const targetPage = context.targetPageUrl.replace(/http(s)?\:\/\//, "");
         const redirectPage = context.redirectUrl.replace(/http(s)?\:\/\//, "");
-        if ((context.targetPageUrl && context.redirectUrl) &&
-            (currentPage !== targetPage && currentPage === redirectPage)) {
-            return;
-        }
+        return (context.targetPageUrl && context.redirectUrl) && (currentPage !== targetPage && currentPage === redirectPage);
+    }
 
-        return new Promise((resolve, reject) => {
-            SalesforceInteractions.cashDom("body").css("visibility", "hidden");
+    /**
+     * @function runTemplateExperience
+     * @param {Object} context
+     * @returns {Promise}
+     * @description Handle
+     */
+    function runTemplateExperience(context) {
+        return new Promise((resolve) => {
+            if (context.userGroup !== "Control") {
+                SalesforceInteractions.cashDom("body").css("visibility", "hidden");
 
-            SalesforceInteractions.mcis.sendStat({
-                campaignStats: [
-                    {
-                        control: false,
+                SalesforceInteractions.mcis.sendStat({
+                    campaignStats: [{
+                        control: context.userGroup === "Control",
                         experienceId: context.experience,
                         stat: "Impression"
-                    }
-                ]
-            });
+                    }]
+                });
 
-            context.paramsForRedirect = (context.maintainQueryParams && window.location.href.match(/\?.*/))
-                ? window.location.href.match(/\?.*/)[0]
-                : "";
+                context.paramsForRedirect = (context.maintainQueryParams && window.location.href.match(/\?.*/))
+                    ? window.location.href.match(/\?.*/)[0]
+                    : "";
 
-            window.location.href = context.redirectUrl + context.paramsForRedirect;
+                window.location.href = context.redirectUrl + context.paramsForRedirect;
+            } else {
+                resolve();
+            }
         });
     }
 
-    function reset(context, template) {
+    function apply(context, template) {
 
+        /** Prevent redirect from occurring while in either the Template Editor or Campaign Editor. */
+        if (isEditorFramePresent()) {
+            return;
+        }
+
+        if (shouldPreventRedirect(context)) {
+            return new Promise((resolve) => false && resolve());
+        }
+
+        return runTemplateExperience(context);
     }
 
-    function control() {
+    function reset(context, template) {
+        // Purposefully left empty
+    }
 
+    function control(context) {
+        if (isEditorFramePresent()) {
+            return;
+        }
+
+        if (shouldPreventRedirect(context)) {
+            return new Promise((resolve) => false && resolve());
+        }
+
+        return runTemplateExperience(context);
     }
 
     registerTemplate({
